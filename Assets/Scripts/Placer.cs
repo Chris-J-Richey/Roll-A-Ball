89737b67e2998c265f380eacf20fa387c74ed7d7
C R <C R>
2016-09-24 09:43:55 -0700

    commiting roll a ball assignment

---
+++ Assets/Scripts/Placer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Placer : MonoBehaviour {
+
+	float x;
+	float y;
+	float z;
+	Vector3 pos;
+
+	void Start(){
+
+		x = Random.Range(-9, 9);
+		y = 0.5f;
+		z = Random.Range(-9, 9);
+		pos = new Vector3(x, y, z);
+		transform.position = pos;
+	}
+}
